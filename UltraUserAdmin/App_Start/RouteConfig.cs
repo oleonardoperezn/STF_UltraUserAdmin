a746c4502ee462028376912c546c81059bcf8a28
Leonardo Perez <oleonardonovelo@gmail.com>
2024-02-01 13:08:30 -0600

    LB <UltraUserAdmin> <Cambio Route.Config.cs>

--- UltraUserAdmin/App_Start/RouteConfig.cs
+++ UltraUserAdmin/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace UltraUserAdmin
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "App", action = "ResetPass", id = UrlParameter.Optional }
             );
         }
     }
